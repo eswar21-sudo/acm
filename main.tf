bb452af394bcc1fb84c1f7b5cc0e8344cbefcfd7
eswar21-sudo <eswarkumar5475@gmail.com>
2025-04-04 13:52:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.region
+}
+
+module "acm_cert" {
+  source = "./Module"
+  primary_domain            = var.primary_domain
+  subject_alternative_names = var.subject_alternative_names
+  validation_method         = var.validation_method
+  key_algorithm             = var.key_algorithm
+  certificate_name          = var.certificate_name
+  tags                      = var.tags
+  create_route53_records    = var.create_route53_records
+  route53_zone_id           = var.route53_zone_id
+  wait_for_validation       = var.wait_for_validation
+  validation_record_fqdns   = var.validation_record_fqdns
+}
